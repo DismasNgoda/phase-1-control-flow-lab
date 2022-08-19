c27ebc137849f3036839188627cf5b9dd0cc33a4
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-19 21:28:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+  let ride;
+  if(a <= 400){
+    ride = "This one is on me!";
+  }
+  else if(a < 2500){
+    ride = "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+  return ride;
+}
+scuberGreetingForFeet(2501);
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+    return city ==="NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'
+  }
 }
